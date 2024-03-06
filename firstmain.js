6086e4eb98e7ed7ffc6c03354e9669e4f4a5d399
Sheryll Anderson <162422268+KathalineForge@users.noreply.github.com>
2024-03-06 09:56:14 +0200

    Create firstmain.js

---
+++ firstmain.js
@@ -0,0 +1,34 @@
+var output = [];
+		
+		this.columns.forEach((column) => {
+			output.push(column.getDefinition(true));
+		});
+		
+		return output;
+	}
+
+var output = [],
+		columns = structured ? this.columns : this.columnsByIndex;
+		
+		columns.forEach((column) => {
+			output.push(column.getComponent());
+		});
+		
+		return output;
+	}
+if(from.parent.isGroup){
+			this._moveColumnInArray(from.parent.columns, from, to, after);
+		}else{
+			this._moveColumnInArray(this.columns, from, to, after);
+		}
+		
+		this._moveColumnInArray(this.columnsByIndex, from, to, after, true);
+		
+		this.rerenderColumns(true);
+		
+		this.dispatch("column-moved", from, to, after);
+		
+		if(this.subscribedExternal("columnMoved")){
+			this.dispatchExternal("columnMoved", from.getComponent(), this.table.columnManager.getComponents());
+		}
+	}
